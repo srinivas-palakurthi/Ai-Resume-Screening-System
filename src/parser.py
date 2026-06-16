784cd5fd140b1bf6a34766b310dd6de118a44321
srinivas-palakurthi <130201966+srinivas-palakurthi@users.noreply.github.com>
2026-06-16 17:08:44 +0530

    parser.py

---
+++ src/parser.py
@@ -0,0 +1,14 @@
+from PyPDF2 import PdfReader
+
+def extract_text(pdf_path):
+    reader = PdfReader(pdf_path)
+
+    text = ""
+
+    for page in reader.pages:
+        page_text = page.extract_text()
+
+        if page_text:
+            text += page_text
+
+    return text
