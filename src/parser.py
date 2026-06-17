0dc7b4207c3e5730dd8ed180c52d18ecc152b0b4
Palakurthi Lakshmi Srinivas <130201966+Lakshmisrinivaspalakurthi@users.noreply.github.com>
2026-06-17 14:04:56 +0530

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -1,11 +1,13 @@
 from pypdf import PdfReader
 
 def extract_text(pdf_path):
+
     reader = PdfReader(pdf_path)
 
     text = ""
 
     for page in reader.pages:
+
         page_text = page.extract_text()
 
         if page_text:
